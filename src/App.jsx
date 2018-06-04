cfe75d4b5b113c17ba810157bb9a327dbe3c3e83
Gavin Quirk <gavinquirk@gmail.com>
2018-06-03 17:55:18 -0700

    reformatted components, moved character array to json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import CharacterDisplay from './Components/CharacterDisplay'
-import Header from './Components/Header'
+import Title from "./components/Title"
+import Wrapper from "./components/Wrapper";
+import characters from './characters.json'
 
-// function handleCharacterClick (event) {
-//     console.log('test')
-// }
+
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      characterArray: [
-        {
-          id: 1,
-          name: 'Aragorn',
-          imagePath: '../images/avatar-aragorn.png',
-          clicked: false
-        },
-        {
-          id: 2,
-          name: 'Legolas',
-          imagePath: '../public/assets/images/avatar-legolas.png',
-          clicked: false
-        },
-        {
-          id: 3,
-          name: 'Gimli',
-          imagePath: '../public/assets/images/avatar-gimli.jpg',
-          clicked: false
-        },
-        {
-          id: 4,
-          name: 'Elrond',
-          imagePath: '../public/assets/images/avatar-elrond.gif',
-          clicked: false
-        },
-        {
-          id: 5,
-          name: 'Gandalf',
-          imagePath: '../public/assets/images/avatar-gandalf.jpg',
-          clicked: false
-        },
-        {
-          id: 6,
-          name: 'Arwen',
-          imagePath: '../public/assets/images/avatar-gandalf.jpg',
-          clicked: false
-        },
-        {
-          id: 7,
-          name: 'Boromir',
-          imagePath: '../public/assets/images/avatar-gandalf.jpg',
-          clicked: false
-        },
-        {
-          id: 8,
-          name: 'Elrond',
-          imagePath: '../public/assets/images/avatar-gandalf.jpg',
-          clicked: false
-        },
-        {
-          id: 9,
-          name: 'Frodo',
-          imagePath: '../public/assets/images/avatar-gandalf.jpg',
-          clicked: false
-        },
-        {
-          id: 10,
-          name: 'Sam',
-          imagePath: '../public/assets/images/avatar-gandalf.jpg',
-          clicked: false
-        },
-        {
-          id: 11,
-          name: 'Merry',
-          imagePath: '../public/assets/images/avatar-gandalf.jpg',
-          clicked: false
-        },
-        {
-          id: 12,
-          name: 'Pippin',
-          imagePath: '../public/assets/images/avatar-gandalf.jpg',
-          clicked: false
-        },
-      ],
-      score : 0
-    }
-  }
+  // Setting this.state.friends to the friends json array
+  state = {
+    characters
+  };
 
-  
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
-    // console.log(this.state.characterArray)
-    // console.log(this.state.score)
     return (
-      <div className='App'>
-        <Header />
-        <div className='CharacterContainer'>
-          {/* <CharacterDisplay score={this.state.score} /> */}
-          <CharacterDisplay score={this.state.score} characterArray={this.state.characterArray} />
-
-        </div>
-      </div>
+      <Wrapper>
+        <Title>Friends List</Title>
+        {/* {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))} */}
+      </Wrapper>
     );
   }
 }
-
 
 export default App;
