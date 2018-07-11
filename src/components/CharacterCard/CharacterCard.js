861a2af22786aa89e4ba50007b0ce629f1eac7be
Gavin Quirk <gavinquirk@gmail.com>
2018-07-10 18:52:11 -0700

    fixed errors

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -3,14 +3,7 @@ import "./CharacterCard.css";
 
 const CharacterCard = props => (
   <div className="card">
-      {/* On Click, handle card click function from app.jsx */}
-<<<<<<< HEAD
       <img onClick={() => props.handleCardClick(props.id)} alt={props.name} src={props.image} />
-      {/* <img onClick={() => props.scoreTest()} alt={props.name} src={props.image} /> */}
-
-=======
-      <img onClick={() => props.handleCardClick(props)} alt={props.name} src={props.image} />
->>>>>>> c8d6c4ee52f7b89e0411cb4d4fba1d4c96de1371
   </div>
 );
 
